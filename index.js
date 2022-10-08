15ebed1530ab1130a9f5b7d3248d683193354fc8
igor <usd.wmz@gmail.com>
2022-10-08 11:47:43 +0200

    create obj dog, cat, woman, man and print values

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { print } from './js/lib.js';
+import { print } from "./js/lib.js";
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
@@ -6,25 +6,53 @@ import { print } from './js/lib.js';
    Web app: _put project's github pages URL here_
    */
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const dog = {
+  species: "dog",
+  name: "Sharik",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "woof-woof!",
+};
+const cat = {
+  species: "cat",
+  name: "Mirzik",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "moow-moow!",
+};
+const woman = {
+  species: "woman",
+  name: "Yulia",
+  gender: "famele",
+  legs: 2,
+  hands: 2,
+  saying: "Hi Ihor!",
+};
+const man = {
+  species: "man",
+  name: "Ihor",
+  gender: "male",
+  legs: 2,
+  hands: 2,
+  saying: "Hello Yulia!",
+};
 
+const inhabitants = [dog, cat, woman, man];
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+inhabitants.forEach((obj) => {
+  print(
+    obj.species +
+      ";" +
+      obj.name +
+      ";" +
+      obj.gender +
+      ";" +
+      obj.legs +
+      ";" +
+      obj.hands +
+      ";" +
+      obj.saying
+  );
+});
